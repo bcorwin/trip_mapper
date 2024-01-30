6ab571312a829568d980db6eff5d40bdc1072501
Benjamin Corwin <bscorwin@gmail.com>
2024-01-30 08:26:28 -0700

    add pre-commit

--- src/ui.R
+++ src/ui.R
@@ -1,22 +1,20 @@
 library(shiny)
 library(leaflet)
 
-# TODO: Add pre-commit hooks
-# TODO: Get changes into GH
 # TODO: Host
 # TODO: Animate travel
 # TODO: Remove extra travel after arrival on travel days
 # TODO: Add eco region to data
-# TODO: Ability to change marker color by: eco region, arrival/departure month, stay length
-# TODO: Ability to change route color: month, length of drive, length of stay
+# TODO: Change marker color by: eco region, arrival/departure month, stay length
+# TODO: Change route color: month, length of drive, length of stay
 # TODO: Marker size -> stay length
 # TODO: Add hiking / activity / other driving days window or zoom in on it
 # TODO: Add pictures from stops
 # TODO: Include rest / fuel / tourist stops and such on travel days?
 
-# source("src/process_data.R", local = new.env())
+if (FALSE) source("src/process_data.R", local = new.env())
 
 shinyUI(fluidPage(
-    titlePanel("Trip MappR"),
-    leafletOutput("map", height = 800)
+  titlePanel("Trip MappR"),
+  leafletOutput("map", height = 800)
 ))
