c711b892df696497cc597a14ab4a8692b098ce16
Benjamin Corwin <bscorwin@gmail.com>
2025-05-24 10:47:01 -0400

    use mouse wheel to change date

--- src/components/RouteMap.tsx
+++ src/components/RouteMap.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, WheelEvent } from "react";
 import { MapContainer, TileLayer } from 'react-leaflet';
 import { Hotline, Color } from 'react-leaflet-hotline';
 import gpxParser from "gpxparser";
@@ -11,10 +11,22 @@ type HotlineValues = {
 
 const position: [number, number] = [38, -98]; // Center of USA
 const gpx_data = "/route.gpx";
-const end_date = new Date('2025-01-01');
 
 export default function RouteMap() {
   const [trackPoints, setTrackPoints] = useState<HotlineValues[]>([]);
+  const [endDate, setEndDate] = useState<Date>(new Date('2023-07-10'));
+
+  function handleWheel(event: WheelEvent<HTMLDivElement>) {
+    event.preventDefault(); // Prevents default scrolling behavior
+    // TODO: Go through travel dates instead?
+    // TODO: Set min / max date (make a var and use it when setting value below too)
+    if(event.deltaY < 0) {
+      setEndDate(new Date(endDate.getTime() - (1000 * 60 * 60 * 24)));
+    } else {
+      setEndDate(new Date(endDate.getTime() + (1000 * 60 * 60 * 24)));
+    }
+    console.log(endDate)
+  };
 
   useEffect(() => {
     const fetchAndParseGpx = async () => {
@@ -28,7 +40,7 @@ export default function RouteMap() {
       gpx.tracks.forEach(track => {
         track.points.forEach(pt => {
           let point_date = new Date(pt.time)
-          if(point_date <= end_date) {
+          if(point_date <= endDate) {
             const val = (Number(point_date) - 1688997780000) / (1728846660000 - 1688997780000);
             points.push({lat: pt.lat, lng: pt.lon, val: val});
           }
@@ -38,41 +50,43 @@ export default function RouteMap() {
     };
 
     fetchAndParseGpx();
-  }, []);
+  }, [endDate]);
 
   return (
-    <MapContainer
-      center={position}
-      zoom={5}
-      style={{ height: "100vh", width: "100%" }}
-      dragging={false}
-      touchZoom={false}
-      scrollWheelZoom={false}
-      doubleClickZoom={false}
-      boxZoom={false}
-      keyboard={false}
-      zoomControl={false}
-    >
-      <TileLayer
-        attribution='&copy; OpenStreetMap contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Hotline
-        data={trackPoints}
-        getLat={({ point }) => point.lat}
-        getLng={({ point }) => point.lng}
-        getVal={({ point }) => point.val}
-        options={{
-          palette: [
-            { r:  27, g: 158, b: 119, t: 0.0 },
-            { r: 217, g:  95, b:   2, t: 0.5 },
-            { r: 117, g: 112, b: 179, t: 1.0 },
-          ] as Color[],
-          weight: 5,
-          outlineWidth: 1,
-          outlineColor: 'black',
-        }}
-      />
-    </MapContainer>
+    <div onWheel={handleWheel}>
+      <MapContainer
+        center={position}
+        zoom={5}
+        style={{ height: "100vh", width: "100%" }}
+        dragging={false}
+        touchZoom={false}
+        scrollWheelZoom={false}
+        doubleClickZoom={false}
+        boxZoom={false}
+        keyboard={false}
+        zoomControl={false}
+      >
+        <TileLayer
+          attribution='&copy; OpenStreetMap contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Hotline
+          data={trackPoints}
+          getLat={({ point }) => point.lat}
+          getLng={({ point }) => point.lng}
+          getVal={({ point }) => point.val}
+          options={{
+            palette: [
+              { r:  27, g: 158, b: 119, t: 0.0 },
+              { r: 217, g:  95, b:   2, t: 0.5 },
+              { r: 117, g: 112, b: 179, t: 1.0 },
+            ] as Color[],
+            weight: 5,
+            outlineWidth: 1,
+            outlineColor: 'black',
+          }}
+        />
+      </MapContainer>
+    </div>
   );
 };
