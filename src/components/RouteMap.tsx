c302370b0f16e48f31340c8abb10c3f97ce7f006
Benjamin Corwin <bscorwin@gmail.com>
2025-05-24 11:31:04 -0400

    add palettes

--- src/components/RouteMap.tsx
+++ src/components/RouteMap.tsx
@@ -16,6 +16,28 @@ const date_range = [
   new Date('2024-10-14')
 ];
 
+const palette1 = [
+  {r: 166, g: 206, b: 227, t: 0.00},
+  {r:  31, g: 120, b: 180, t: 0.33},
+  {r: 178, g: 223, b: 138, t: 0.66},
+  {r:  51, g: 160, b:  44, t: 1.00},
+]
+
+const palette2 = [
+  { r:  27, g: 158, b: 119, t: 0.0 },
+  { r: 217, g:  95, b:   2, t: 0.5 },
+  { r: 117, g: 112, b: 179, t: 1.0 },
+]
+
+const palette3 = [
+  { r: 237, g: 248, b: 251, t: 0.0 },
+  { r: 191, g: 211, b: 230, t: 0.2 },
+  { r: 158, g: 188, b: 218, t: 0.4 },
+  { r: 140, g: 150, b: 198, t: 0.6 },
+  { r: 136, g:  86, b: 167, t: 0.8 },
+  { r: 129, g:  15, b: 124, t: 1.0 },
+]
+
 function clamp(val:number, min:number, max:number) {
   return Math.min(Math.max(val, min), max);
 }
@@ -50,7 +72,8 @@ export default function RouteMap() {
         track.points.forEach(pt => {
           let point_date = new Date(pt.time)
           if(point_date <= endDate) {
-            const val = (Number(point_date) - 1688997780000) / (1728846660000 - 1688997780000);
+            const val = (Number(point_date) - Number(date_range[0])) /
+              (Number(date_range[1]) - Number(date_range[0]));
             points.push({lat: pt.lat, lng: pt.lon, val: val});
           }
         });
@@ -85,13 +108,9 @@ export default function RouteMap() {
           getLng={({ point }) => point.lng}
           getVal={({ point }) => point.val}
           options={{
-            palette: [
-              { r:  27, g: 158, b: 119, t: 0.0 },
-              { r: 217, g:  95, b:   2, t: 0.5 },
-              { r: 117, g: 112, b: 179, t: 1.0 },
-            ] as Color[],
+            palette: palette1 as Color[],
             weight: 5,
-            outlineWidth: 1,
+            outlineWidth: 10,
             outlineColor: 'black',
           }}
         />
